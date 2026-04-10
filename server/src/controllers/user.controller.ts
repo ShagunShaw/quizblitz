d957a56baccda28162ae353c197987d799bf6756
Shagun Shaw <shagunshaw2006@gmail.com>
2026-04-11 03:36:06 +0530

    Implement Google OAuth for co-host invitation acceptance and create quiz dashboard

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,12 +2,74 @@ import { Quiz } from "../schemas/quiz.schema.ts";
 import { User } from "../schemas/user.schema.ts";
 import { sendNewUser, sendExistingUser } from "../emailService/sendEmail.ts"
 import jwt from 'jsonwebtoken'
+import axios from 'axios'
+import { generateAccessToken, generateRefreshToken } from "../auth/generateAndVerifyToken.ts";
 
-export const logOutUser= async (req, res) => {
+export const googleRedirect = (req, res) => {
+    const redirectUri = 'http://localhost:3000/auth/google/callback';
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=email profile&access_type=offline&prompt=consent`;
+
+    console.log("Response from /auth/google\n", url)
+
+    res.redirect(url);
+}
+
+export const googleCallback = async (req, res) => {
+    const code = req.query.code;
+    const redirectUri = 'http://localhost:3000/auth/google/callback';
+
     try {
-        const payload= req.user;
+        const tokenRes = await axios.post('https://oauth2.googleapis.com/token', {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            code,
+            redirect_uri: redirectUri,
+            grant_type: 'authorization_code',
+        });
 
-        const user= await User.findById(payload.userId)
+        const accessTokenByGoogle = tokenRes.data.access_token;
+
+        const profileRes = await axios.get(
+            'https://www.googleapis.com/oauth2/v2/userinfo',
+            {
+                headers: { Authorization: `Bearer ${accessTokenByGoogle}` },
+            }
+        );
+
+        console.log('🔗 Google User:', {
+            name: profileRes.data.name,
+            email: profileRes.data.email,
+        });
+
+        const user = await User.findOneAndUpdate(
+            { email: profileRes.data.email },
+            { $setOnInsert: { username: profileRes.data.name, email: profileRes.data.email } },
+            { upsert: true, new: true }
+        )
+
+        const sessionId = Date.now() + Math.random()
+        const accessToken = generateAccessToken(user._id, profileRes.data.email, sessionId)
+        const refreshToken = generateRefreshToken(user._id)
+
+        user.refreshTokens.push({ sessionId, token: refreshToken })
+        await user.save()
+
+        res.cookie('accessToken', accessToken)
+        res.cookie('refreshToken', refreshToken)
+        res.redirect('http://localhost:5500/client/success.html');
+    } catch (err) {
+        console.error('Google OAuth Error:', err);
+        res.send('Google login failed');
+    }
+}
+
+export const logOutUser = async (req, res) => {
+    try {
+        const payload = req.user;
+
+        const user = await User.findById(payload.userId)
         user.refreshTokens.pull({ sessionId: payload.sessionId })
         await user.save()
 
@@ -15,43 +77,122 @@ export const logOutUser= async (req, res) => {
         res.clearCookie('refreshToken')
         res.status(200).send("User logged out successfully").redirect("http://localhost:5500/client/logout.html")
     } catch (error) {
-        res.status(500).json({errorCode: error.code, errorMessage: error.message})
+        res.status(500).json({ errorCode: error.code, errorMessage: error.message })
     }
 }
 
-export const sendEmail= async (req, res) => {
+export const sendEmail = async (req, res) => {
     try {
-        const { quizId }= req.params
+        const { quizId } = req.params
 
-        const quiz= await Quiz.findById(quizId)
-        if(quiz.Hosts.length === 3) {
+        const quiz = await Quiz.findById(quizId)
+        if (quiz.Hosts.length === 3) {
             res.status(400).send("Not more than 3 hosts can be there for a quiz")
         }
 
-        const user= await User.findById(req.user.userId)
-        
-        const { coHostEmail }= req.body
-        const coHost= await User.find({email: coHostEmail})
-        const subject= "You've been invited to co-host a quiz on QuizBlitz!"
+        const user = await User.findById(req.user.userId)
+
+        const { coHostEmail } = req.body
+        const coHost = await User.find({ email: coHostEmail })
+
+        const subject = "You've been invited to co-host a quiz on QuizBlitz!"
 
         type JwtExpiry = `${number}d` | `${number}h` | `${number}m` | `${number}s`
-        const payload= {quizId, coHostEmail, role: 'cohost'}
-        const token= jwt.sign(payload, process.env.COHOST_TOKEN_SECRET, {expiresIn: process.env.COHOST_TOKEN_EXPIRY as JwtExpiry, algorithm: 'HS256'})
-        const acceptUrl= `http://localhost:5500/client/acceptInvite.html?token=${token}`         // Update this with your react route later
+        const payload = { quizId, coHostEmail, role: 'cohost' }
+        const token = jwt.sign(payload, process.env.COHOST_TOKEN_SECRET, { expiresIn: process.env.COHOST_TOKEN_EXPIRY as JwtExpiry, algorithm: 'HS256' })
+        const acceptUrl = `http://localhost:5500/client/acceptInvite.html?token=${token}`         // Update this with your react route later
 
-        if(!coHost) {
+        if (!coHost) {
             sendNewUser(coHostEmail, subject, user.username, quiz.Title, acceptUrl)
         }
         else {
+            if(await quiz.findOne({ Hosts: coHost._id }))       // fix this to check if this co-host is already a cohost of the given quiz id or not
+            
             sendExistingUser(coHostEmail, subject, user.username, quiz.Title, acceptUrl)
         }
 
         res.status(200).send("Email sent successfully!")
     } catch (error) {
-        res.status(500).json({errorCode: error.code, errorMessage: error.message})
+        res.status(500).json({ errorCode: error.code, errorMessage: error.message })
     }
 }
 
-export const acceptEmail= async (req, res) => {
-    
+export const acceptEmail = async (req, res) => {
+    const redirectUri = 'http://localhost:3000/co-host/accept/callback';
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const { token } = req.params
+
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&state=${token}&response_type=code&scope=email profile&access_type=offline&prompt=consent`;          // 'state=${token}' is where we are passing our token to be extracted in the callback route using 'req.query.state'
+
+    console.log("Response from /co-host/accept/:token\n", url)
+
+    res.redirect(url);
+}
+
+
+interface IInvitePayload {
+    quizId: string
+    coHostEmail: string
+    role: string
+}
+
+export const acceptCallback = async (req, res) => {
+    const code = req.query.code;
+    const token = req.query.state;
+    const redirectUri = 'http://localhost:3000/co-host/accept/callback';
+
+    try {
+        const tokenRes = await axios.post('https://oauth2.googleapis.com/token', {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            code,
+            redirect_uri: redirectUri,
+            grant_type: 'authorization_code',
+        });
+
+        const accessTokenByGoogle = tokenRes.data.access_token;
+
+        const profileRes = await axios.get(
+            'https://www.googleapis.com/oauth2/v2/userinfo',
+            {
+                headers: { Authorization: `Bearer ${accessTokenByGoogle}` },
+            }
+        );
+
+        console.log('🔗 Google User:', {
+            name: profileRes.data.name,
+            email: profileRes.data.email,
+        });
+
+        const payload = jwt.verify(token, process.env.COHOST_TOKEN_SECRET) as IInvitePayload
+        if (payload.coHostEmail !== profileRes.data.email) {
+            res.status(400).send("You must login with the email to which the invite has been sent!!")
+        }
+
+        const user = await User.findOneAndUpdate(
+            { email: payload.coHostEmail },
+            { $setOnInsert: { username: profileRes.data.name, email: profileRes.data.email } },
+            { upsert: true, new: true }
+        )
+
+        const quiz = await Quiz.findById(payload.quizId)
+        quiz.Hosts.push(user._id)
+        await quiz.save()
+
+        const sessionId = Date.now() + Math.random()
+        const accessToken = generateAccessToken(user._id, profileRes.data.email, sessionId)
+        const refreshToken = generateRefreshToken(user._id)
+
+        user.refreshTokens.push({ sessionId, token: refreshToken })
+        await user.save()
+
+        res.cookie('accessToken', accessToken)
+        res.cookie('refreshToken', refreshToken)
+
+        res.status(200)
+           .redirect('http://localhost:5500/quizDashboard.html')     
+    } catch (err) {
+        console.error('Co-Host OAuth Error:', err);
+        res.send('Co-Host adding failed');
+    }
 }
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,74 +1,19 @@
 import express, { Router } from "express";
-import axios from 'axios'
 import { User } from "../schemas/user.schema.ts";
-import { generateAccessToken, generateRefreshToken, verifyAccessToken } from "../auth/generateAndVerifyToken.ts";
-import { sendEmail, logOutUser, acceptEmail } from "../controllers/user.controller.ts";
+import { verifyAccessToken } from "../auth/generateAndVerifyToken.ts";
+import { sendEmail, logOutUser, acceptEmail, googleRedirect, googleCallback, acceptCallback } from "../controllers/user.controller.ts";
 const router: Router= express.Router()
 
-router.get('auth/google', (req, res) => {
-  const redirectUri = 'http://localhost:3000/auth/google/callback';
-  const clientId = process.env.GOOGLE_CLIENT_ID;
+router.get('auth/google', googleRedirect);
 
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=email profile&access_type=offline&prompt=consent`;
-
-  console.log("Response from /auth/google\n", url)
-
-  res.redirect(url);
-});
-
-router.get('auth/google/callback', async (req, res) => {
-  const code = req.query.code;
-  const redirectUri = 'http://localhost:3000/auth/google/callback';
-
-  try {
-    const tokenRes = await axios.post('https://oauth2.googleapis.com/token', {
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      code,
-      redirect_uri: redirectUri,
-      grant_type: 'authorization_code',
-    });
-
-    const accessTokenByGoogle = tokenRes.data.access_token;
-
-    const profileRes = await axios.get(
-      'https://www.googleapis.com/oauth2/v2/userinfo',
-      {
-        headers: { Authorization: `Bearer ${accessTokenByGoogle}` },
-      }
-    );
-
-    console.log('🔗 Google User:', {
-      name: profileRes.data.name,
-      email: profileRes.data.email,
-    });
-
-    const user= await User.findOneAndUpdate(
-      {email: profileRes.data.email},
-      {$setOnInsert: {username: profileRes.data.name, email: profileRes.data.email}},
-      {upsert: true, new: true}
-    )
-
-    const sessionId= Date.now() + Math.random()
-    const accessToken= generateAccessToken(user._id, profileRes.data.email, sessionId)
-    const refreshToken= generateRefreshToken(user._id)
-
-    user.refreshTokens.push({sessionId, token: refreshToken})
-    await user.save()
-
-    res.cookie('accessToken', accessToken)
-    res.cookie('refreshToken', refreshToken)
-    res.redirect('http://localhost:5500/client/success.html');
-  } catch (err) {
-    console.error('Google OAuth Error:', err);
-    res.send('Google login failed');
-  }
-})
+router.get('auth/google/callback', googleCallback)
 
 router.post('user/logout/', verifyAccessToken, logOutUser)
 
 router.post('/co-host/:quizId', verifyAccessToken, sendEmail)     // send email to the co-host to accept the invite
 
-router.post('/co-host/accept' jdbj)    // This route will be called by frontend
+router.post('/co-host/accept/:token', acceptEmail)    // This route will be called by frontend
+
+router.post('/co-host/accept/callback', acceptCallback)
 
 export default router;
