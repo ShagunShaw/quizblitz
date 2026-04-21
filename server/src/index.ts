071135223ded22f35d2a3ee1a93e5307e83aef56
Shagun Shaw <shagunshaw2006@gmail.com>
2026-04-22 03:11:40 +0530

    feat: add node-cron for scheduled quiz cleanup and enhance quiz result calculation logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config';
-import cookieParser  from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import UserRouter from './routes/user.route.ts'
 import QuizRouter from './routes/quiz.route.ts'
 import connectDB from './db.ts'
 import { initSocket } from './socket.ts';
 import http from 'http'
+import cron from 'node-cron'
+import { Quiz } from './schemas/quiz.schema.ts';
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const server= http.createServer(app)
+const server = http.createServer(app)
 
 await connectDB()
 initSocket(server)
@@ -24,4 +26,14 @@ app.use("/api/v1/quiz/", QuizRouter)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
+})
+
+cron.schedule('0 3 * * *', async () => {
+  // if this application scales in production, then we an maintain an array sort of things in our database which contain all the quiz id's that had been attempted and are not permanent. As the 'endQuiz' event is triggered in socket.io, we add values to that array and at time of the cron job, just retrieve all values from the array and perform 'findByIdAndDelete()'. For now, we are not adding anything as such.
+  const response = await Quiz.deleteMany({
+    isAttempted: true,
+    isPermanent: false
+  });
+
+  console.log(`${response.deletedCount} quizzes deleted at ${new Date()}`);
 })
--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,4 +1,5 @@
 import { Server } from "socket.io"
+import { Quiz } from "./schemas/quiz.schema.ts";
 
 interface QuestionData {
     time: number,
@@ -28,80 +29,97 @@ const initSocket = (server) => {
     io = new Server(server)
 
     function calculateResult(roomCode, correctOption) {
-        const data = quizRooms[roomCode]
-        let count = 0
+        try {
+            const data = quizRooms[roomCode]
+            let count = 0
+            const stats = data.currentQuesStats      // which has currently {} in its default value
 
-        for (const val of data.submissions) {
-            let givenMarks = 0
+            for (const val of data.submissions) {
+                let givenMarks = 0
 
-            // how this will handle millions of users in such a case, where it can to scan everytime for a new id?
-            const player: Scores = data.scores.find(p => p.id === val.id)
+                // how this will handle millions of users in such a case, where it can to scan everytime for a new id?
+                const player: Scores = data.scores.find(p => p.id === val.id)
 
-            // give marks to each user accordingly
-            if (val.submittedOption && val.submittedOption === correctOption) {
+                if (val.submittedOption) {
+                    stats[val.submittedOption] = (!stats[val.submittedOption]) ? 1 : (stats[val.submittedOption] + 1)
+                    data.totalSubmission += 1
+                }
 
-                if (val.isAutoSubmit) {
-                    givenMarks = 1
+                // give marks to each user accordingly
+                if (val.submittedOption && val.submittedOption === correctOption) {
+                    if (val.isAutoSubmit) {
+                        givenMarks = 1
+                        if (player) player.streak = 0
+                    }
+                    else {
+                        count++;
+
+                        if (count == 1) givenMarks = 15;
+                        else if (count == 2) givenMarks = 14;
+                        else if (count == 3) givenMarks = 13;
+                        else if (count == 4) givenMarks = 12;
+                        else if (count == 5) givenMarks = 11;
+                        else if (count == 6) givenMarks = 10;
+                        else if (count == 7) givenMarks = 9;
+                        else if (count >= 8 && count <= 17) givenMarks = 7
+                        else if (count >= 18 && count <= 32) givenMarks = 5
+                        else givenMarks = 3
+
+                        if (player) player.streak += 1
+
+                        if (player.streak == 3) givenMarks += 3
+                        if (player.streak == 5) givenMarks += 4
+                        if (player.streak == 10) givenMarks += 7
+                    }
+                }
+                else {
                     if (player) player.streak = 0
                 }
-                else {
-                    count++;
 
-                    if (count == 1) givenMarks = 15;
-                    else if (count == 2) givenMarks = 14;
-                    else if (count == 3) givenMarks = 13;
-                    else if (count == 4) givenMarks = 12;
-                    else if (count == 5) givenMarks = 11;
-                    else if (count == 6) givenMarks = 10;
-                    else if (count == 7) givenMarks = 9;
-                    else if (count >= 8 && count <= 17) givenMarks = 7
-                    else if (count >= 18 && count <= 32) givenMarks = 5
-                    else givenMarks = 3
-
-                    if (player) player.streak += 1
-
-                    if (player.streak == 3) givenMarks += 3
-                    if (player.streak == 5) givenMarks += 4
-                    if (player.streak == 10) givenMarks += 7
-                }
-            }
-            else {
-                if (player) player.streak = 0
-            }
-
-            player.currentPoint = givenMarks + player.currentPoint
-            // for updating the top7 and topPoints array
-            if (data.topPoints.length === 7) {
-                if (player.currentPoint > data.topPoints[6]) {
-                    for (let i = 0; i < data.topPoints.length; i++) {
-                        if (data.topPoints[i] < player.currentPoint) {
-                            data.topPoints.splice(i, 0, player.currentPoint)
-                            data.top7.splice(i, 0, player.username)
-                            break
+                player.currentPoint = givenMarks + player.currentPoint
+                // for updating the top7 and topPoints array
+                if (data.topPoints.length === 7) {
+                    if (player.currentPoint > data.topPoints[6]) {
+                        for (let i = 0; i < data.topPoints.length; i++) {
+                            if (data.topPoints[i] < player.currentPoint) {
+                                data.topPoints.splice(i, 0, player.currentPoint)
+                                data.top7.splice(i, 0, player.username)
+                                break
+                            }
                         }
-                    }
-                    data.top7.pop()
-                    data.topPoints.pop()
-                }
-            }
-            else {
-                if (player.currentPoint > data.topPoints[data.topPoints.length - 1]) {
-                    for (let i = 0; i < data.topPoints.length; i++) {
-                        if (data.topPoints[i] < player.currentPoint) {
-                            data.topPoints.splice(i, 0, player.currentPoint)
-                            data.top7.splice(i, 0, player.username)
-                            break
-                        }
+                        data.top7.pop()
+                        data.topPoints.pop()
                     }
                 }
                 else {
-                    data.topPoints.push(player.currentPoint)
-                    data.top7.push(player.username)
+                    if (player.currentPoint > data.topPoints[data.topPoints.length - 1]) {
+                        for (let i = 0; i < data.topPoints.length; i++) {
+                            if (data.topPoints[i] < player.currentPoint) {
+                                data.topPoints.splice(i, 0, player.currentPoint)
+                                data.top7.splice(i, 0, player.username)
+                                break
+                            }
+                        }
+                    }
+                    else {
+                        data.topPoints.push(player.currentPoint)
+                        data.top7.push(player.username)
+                    }
                 }
             }
+
+            data.isResultCalculated = true
+        } catch (error) {
+            console.log("Something went wrong: \n", error)
         }
+    }
 
-        data.isResultCalculated = true
+    function calculatePercent(roomCode) {
+        const data = quizRooms[roomCode]
+
+        for (let [key, value] of Object.entries(data.currentQuesStats)) {
+            data.currentQuesStats[key] = Math.round(((value as number) / (data.totalSubmission)) * 100)
+        }
     }
 
     io.on("connection", (socket) => {
@@ -129,7 +147,9 @@ const initSocket = (server) => {
                     timer: null,
                     scores: [],
                     top7: [],
-                    topPoints: []
+                    topPoints: [],
+                    currentQuesStats: {},
+                    totalSubmission: 0      // in case some submissions do not have any option selected, so for that we cant count submissions.length as my valid submissions count
                 }
             }
 
@@ -156,29 +176,36 @@ const initSocket = (server) => {
                     timer: null,
                     scores: [],
                     top7: [],
-                    topPoints: []
+                    topPoints: [],
+                    currentQuesStats: {},
+                    totalSubmission: 0
                 }
             }
         })
 
         socket.on("publishQuestion", ({ roomCode, questionData }) => {
-            if(!quizRooms[roomCode]) return
+            if (!quizRooms[roomCode]) return
 
             // subsequent questions — only update these, keep scores intact
             quizRooms[roomCode].currentQuestion = questionData as QuestionData
             quizRooms[roomCode].submissions = [] as Submissions[]
             quizRooms[roomCode].timer = null
             quizRooms[roomCode].isResultCalculated = false
+            quizRooms[roomCode].currentQuesStats = {}
+            quizRooms[roomCode].totalSubmission = 0
 
 
             io.to(roomCode).emit("newQuestion", questionData)
 
             quizRooms[roomCode].timer = setTimeout(() => {
-                if(!quizRooms[roomCode].isResultCalculated)    calculateResult(roomCode, questionData.correctOption)
+                if (!quizRooms[roomCode].isResultCalculated) {
+                    calculateResult(roomCode, questionData.correctOption)
+                    calculatePercent(roomCode)
+                }
                 const player: Scores = quizRooms[roomCode].scores.find(p => p.id === socket.id)
 
                 // see how we made use of option chaining properly; If player is undefined, doing player.username will throw 'Cannot read properties of undefined' before even reaching the || 'Host' fallback.
-                io.to(roomCode).emit("displayScoreBoard", { username: player?.username || 'Host', userscore: player?.currentPoint ?? NaN, top7: quizRooms[roomCode].top7, topPoints: quizRooms[roomCode].topPoints })
+                io.to(roomCode).emit("displayScoreBoard", { username: player?.username || 'Host', userscore: player?.currentPoint ?? NaN, top7: quizRooms[roomCode].top7, topPoints: quizRooms[roomCode].topPoints, optionStats: quizRooms[roomCode].currentQuesStats, correctOption: questionData.correctOption })
             }, (questionData.time + 2) * 1000)
         })
 
@@ -195,6 +222,21 @@ const initSocket = (server) => {
                 isAutoSubmit: isAutoSubmit as boolean
             }
             quizRooms[roomCode].submissions.push(data)
+        })
+
+        socket.on("endQuiz", async (roomCode) => {
+            const quiz = await Quiz.findOneAndUpdate(
+                { roomCode: roomCode },
+                { $set: { isAttempted: true } },
+                { returnDocument: "after" }
+            );
+
+            delete quizRooms[roomCode]
+
+            io.to(roomCode).emit("quizEnded", {
+                finalTop7: quizRooms[roomCode].top7,
+                finalTopPoints: quizRooms[roomCode].topPoints
+            })
         })
 
         socket.on("disconnect", () => {
