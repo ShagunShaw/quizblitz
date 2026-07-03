8888811a688040befe59d0497670b69e0b88ab0e
Shagun Shaw <shagunshaw2006@gmail.com>
2026-07-04 01:28:19 +0530

    Prepare backend for production deployment

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 // maybe this URI needs to be changed while in production (Check that)
-const MONGO_URI = `${process.env.MONGODB_URL}/${process.env.DATABASE_NAME}?authSource=admin`;
+// const MONGO_URI = `${process.env.MONGODB_URL}/${process.env.DATABASE_NAME}?authSource=admin`;
+const MONGO_URI = (process.env.NODE_ENV === 'development')?`${process.env.MONGODB_URL}/${process.env.DATABASE_NAME}?authSource=admin`:process.env.MONGO_ATLAS_URI;
 
 const connectDB = async () => {
   try {
